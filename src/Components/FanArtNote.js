c397375bf0b9842e0e2db0d09b2d0800deb8741e
moraelhumano <moraelhumano@gmail.com>
2021-10-20 20:58:49 -0500

    banner fan art was modificated

--- src/Components/FanArtNote.js
+++ src/Components/FanArtNote.js
@@ -9,8 +9,7 @@ class FanArtNote extends Component{
         <a href="https://reycamiseta.com/?page_id=3751" target="_blank">
         <div className="w-screen bg-black px-2 py-4 flex items-center justify-around flex-col md:flex-row text-center">
             <div>
-                <p className="text-white merch-banner-subtext text-base pt-2 md:pt-0 ">ESTE ES UN FANART, NO ES EL SITIO OFICIAL DE</p>
-                <p className="text-white merch-banner-text text-lg md:text-4xl ">
+                <p className="text-white merch-banner-subtext text-base pt-2 md:pt-0 ">ESTE ES UN FANART, NO ES EL SITIO OFICIAL DE
                     CHAVXS BANDA
                 </p>
             </div>
